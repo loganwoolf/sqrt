a5ca04b3ee6a5d45324c61d1c9ed51882d9b07b9
Logan Woolf <loganwoolf@gmail.com>
2025-07-01 01:32:28 -0600

    Set up Key

---
+++ src/components/Keypad/Key/index.tsx
@@ -0,0 +1,46 @@
+import type { ShiftState } from "..";
+import { cn } from "../../../lib/utils";
+
+interface KeyAction {
+	label: string;
+	onClick: () => void;
+}
+
+export interface KeyActions {
+	main: KeyAction;
+	shift?: KeyAction;
+	classes?: string;
+}
+
+type ButtonType =
+	| "clear"
+	| "memory"
+	| "number"
+	| "shift"
+	| "small"
+	| "stack"
+	| "wide";
+
+interface KeyProps {
+	params: KeyActions;
+	shiftActive: ShiftState;
+	buttonType: ButtonType | ButtonType[];
+}
+
+export default function Key({ params, shiftActive, buttonType }: KeyProps) {
+	return (
+		<div className={cn('key', params.classes)}>
+			{params.shift?.label && <span className="">{params.shift.label}</span>}
+			<button
+				type="button"
+				className={cn(
+					"p-2 w-full",
+					Array.isArray(buttonType) ? buttonType.join(" ") : buttonType,
+				)}
+				onClick={!shiftActive ? params.main.onClick : params.shift?.onClick}
+			>
+				{params.main.label}
+			</button>
+		</div>
+	);
+}
