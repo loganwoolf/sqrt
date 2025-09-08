5ae54f36643bd972a0d3cdf345bf77c560267344
Logan Woolf <logan.woolf@arcticleaf.io>
2025-09-08 08:04:11 -0600

    Add line numbers to stack and offset when buffer active

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -8,15 +8,21 @@ export default function Display({ buffer }: DisplayProps) {
 	const { state } = useAppContext();
 
 	return (
-		<div class="display grid grid-rows-[1fr_auto] w-full border-2 min-h-32 mb-2">
-			<div className="stack bg-cyan-200 flex flex-col-reverse font-mono leading-none">
+		<div class="display grid grid-rows-[1fr_auto] w-full border-2 h-[9lh] mb-2 font-mono text-right">
+			<div className="stack bg-cyan-200 flex flex-col-reverse">
 				{state.slice(0, buffer ? 7 : 8).map((line, index) => (
-					<div class="stack-item" key={`${index}-${line}`}>
-						{line}
+					<div className="grid grid-cols-[1fr_2rem]" key={`${index}-${line}`}>
+						<div class="stack-item">{line}</div>
+						<div className="">:{buffer ? index + 1 : index}</div>
 					</div>
 				))}
 			</div>
-			{buffer && <div className="buffer bg-amber-200">{buffer}</div>}
+			{buffer && (
+				<div className="grid grid-cols-[1fr_2rem] bg-amber-200">
+					<div className="buffer">{buffer}</div>
+					<div>{buffer ? ":0" : null}</div>
+				</div>
+			)}
 		</div>
 	);
 }
