2cea27e0cc8cc01cac5d9441095a053db29cb293
Logan Woolf <loganwoolf@gmail.com>
2025-09-19 07:23:21 -0600

    Add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+	return (
+		<div className="mt-1 flex">
+			<h1>calculatrix</h1>
+			<a className={"ml-auto"} href="https://github.com/loganwoolf/calculatrix">
+				help
+			</a>
+		</div>
+	);
+}
