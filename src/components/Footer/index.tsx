def741a81ebd991e2753ce8c27bb2bf0981e9832
Logan Woolf <loganwoolf@gmail.com>
2025-09-19 07:27:11 -0600

    rename to sqrt

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<div className="mt-1 flex">
-			<h1>calculatrix</h1>
+			<h1 className={"font-bold"}>sqrt</h1>
 			<a className={"ml-auto"} href="https://github.com/loganwoolf/calculatrix">
 				help
 			</a>
