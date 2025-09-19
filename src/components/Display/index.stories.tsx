faf765e22b0de4ab74466dbea28700d770a8c9d8
Logan Woolf <loganwoolf@gmail.com>
2025-09-19 07:23:00 -0600

    Update stories to use satisfies

--- src/components/Display/index.stories.tsx
+++ src/components/Display/index.stories.tsx
@@ -1,23 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/preact-vite";
-import type { ComponentProps } from "preact";
-import type { Stack } from "../../AppContext";
-import { AppContext } from "../../lib/storybook/decorators";
+import { AppContext, ShiftContext } from "../../lib/storybook/decorators";
 import Display from ".";
-
-type StoryArgs = ComponentProps<typeof Display> & {
-	stack?: Stack;
-};
 
 const meta = {
 	component: Display,
-	decorators: [AppContext],
+	decorators: [ShiftContext, AppContext],
 	args: {
 		stack: ["789", "456", "123"],
 		buffer: "437378",
 	},
-} satisfies Meta<StoryArgs>;
+} satisfies Meta<typeof Display>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Main: Story = {};
+export const Main = {} satisfies Story;
