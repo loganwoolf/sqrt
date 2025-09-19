cd1c15d1315bfc33ad873d8277efd2813235dd68
Logan Woolf <loganwoolf@gmail.com>
2025-09-19 07:22:43 -0600

    Add Statusline to app

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import "./app.css";
 import { useState } from "preact/hooks";
 import { AppContextProvider } from "./AppContext";
 import Display from "./components/Display";
+import Footer from "./components/Footer";
 import Keypad from "./components/Keypad";
 import { ShiftContextProvider } from "./ShiftContext";
 
@@ -12,13 +13,14 @@ export function App() {
 
 	return (
 		<div className="grid min-h-dvh place-content-center">
-			<div className="calculator grid h-dvh max-h-[36rem] w-screen max-w-sm grid-rows-[minmax(0,1fr)_auto_auto] border-2 border-black p-4">
+			<div className="calculator grid h-dvh max-h-[36rem] w-screen max-w-sm grid-rows-[minmax(0,1fr)_auto_auto] border-2 border-black p-4 pb-2">
 				<AppContextProvider>
-					<Display buffer={buffer} />
 					<ShiftContextProvider>
+						<Display buffer={buffer} />
 						<Keypad buffer={buffer} setBuffer={setBuffer} />
 					</ShiftContextProvider>
 				</AppContextProvider>
+				<Footer />
 			</div>
 		</div>
 	);
--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,4 +1,5 @@
 import { useAppContext } from "../../AppContext";
+import Statusline from "./Statusline";
 
 interface DisplayProps {
 	buffer: string | null;
@@ -8,8 +9,11 @@ export default function Display({ buffer }: DisplayProps) {
 	const { state } = useAppContext();
 
 	return (
-		<div class="display mb-2 grid w-full grid-rows-[1fr_auto] border-2 text-right font-mono">
-			<div className="stack flex flex-col-reverse overflow-auto bg-cyan-200">
+		<div class="display mb-2 grid w-full grid-rows-[1fr_auto] border-2 bg-cyan-200 font-mono">
+			<div className="mx-2">
+				<Statusline />
+			</div>
+			<div className="stack flex flex-col-reverse overflow-auto text-right">
 				{state.map((line, index) => (
 					<div
 						className="grid grid-cols-[2rem_1fr] pr-2"
@@ -21,7 +25,7 @@ export default function Display({ buffer }: DisplayProps) {
 				))}
 			</div>
 			{buffer && (
-				<div className="buffer grid grid-cols-[2rem_1fr] bg-amber-200 pr-2">
+				<div className="buffer grid grid-cols-[2rem_1fr] bg-amber-200 pr-2 text-right">
 					<div>{buffer ? "0:" : null}</div>
 					<div className="buffer-item">{buffer}</div>
 				</div>
