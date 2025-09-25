76ccc3cbf448ef55332d0320e1b3092909800cea
Logan Woolf <loganwoolf@gmail.com>
2025-09-25 01:28:06 -0600

    Format stack line display

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -18,10 +18,9 @@ export default function Display({ buffer }: { buffer: Buffer }) {
 						className="grid grid-cols-[2rem_1fr] pr-2"
 						key={`${index}-${line}`}
 					>
-						<div className="">{index}:</div>
-						<div class="stack-item">
-							{new Intl.NumberFormat().format(+line)}
-						</div>
+						<div>{stackRowName(index)}:</div>
+						{/* @ts-expect-error format actually does accept a string */}
+						<div class="stack-item">{formatter.format(line)}</div>
 					</div>
 				))}
 			</div>
@@ -33,4 +32,27 @@ export default function Display({ buffer }: { buffer: Buffer }) {
 			)}
 		</div>
 	);
+}
+
+const formatter = new Intl.NumberFormat(undefined, {
+	maximumFractionDigits: 8,
+	maximumSignificantDigits: 16,
+	// @ts-expect-error roundingPriority is valid key https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#options
+	roundingPriority: "lessPrecision",
+	trailingZeroDisplay: "stripIfInteger",
+});
+
+function stackRowName(index: number): string {
+	switch (index) {
+		case 0:
+			return "X";
+		case 1:
+			return "Y";
+		case 2:
+			return "Z";
+		case 3:
+			return "T";
+		default:
+			return (index + 1).toString();
+	}
 }
