2a7296f0f223005496dc04d57b926eb67f0d139d
Logan Woolf <loganwoolf@gmail.com>
2025-09-24 21:40:36 -0600

    Lint fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,10 @@ export default function Footer() {
 	return (
 		<div className="mt-1 flex">
 			<h1 className={"font-bold"}>sqrt</h1>
-			<a className={"ml-auto"} href="https://github.com/loganwoolf/sqrt?tab=readme-ov-file#sqrt">
+			<a
+				className={"ml-auto"}
+				href="https://github.com/loganwoolf/sqrt?tab=readme-ov-file#sqrt"
+			>
 				help
 			</a>
 		</div>
